adc540c112bdd67ca371f12d6e547bc4d976a63a
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2022-04-18 13:46:42 +0200

    Update post-fs-data.sh
    zram

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -1,3 +1,8 @@
+MODDIR=${0%/*}
+setenforce 0
+setprop ro.vendor.qti.config.zram true
+write /proc/sys/vm/page-cluster 0
+write /sys/block/zram0/max_comp_streams 4
 conflict=$(xml=$(find /data/adb -iname "*.xml");for i in $xml; do if grep -q 'allow-in-power-save package="com.google.android.gms"' $i 2>/dev/null; then echo "$i";fi; done)
  for i in $conflict
  do
